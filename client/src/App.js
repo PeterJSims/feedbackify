702846c802b493619984d69e836e30ba158847eb
Peter Sims <ragtimesims@gmail.com>
2019-09-01 11:09:03 -0400

    react added in and proxy set up

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<a href="/auth/google">Sign In With Google</a>
+		</div>
+	);
+}
+
+export default App;
