28bc2a9acf8e5e5d3080ddfeb552d777ac6351ff
Peter Sims <ragtimesims@gmail.com>
2019-09-01 20:32:07 -0400

    startingmaterialize-css implementation

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<a href="" className="left brand-logo">
+						Feedbackify{' '}
+					</a>
+
+					<ul className="right">
+						<li>
+							<a href="">Login With Google</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+export default Header;
