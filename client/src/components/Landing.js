457aa88df3d1f888bd566f9a92a736d2268fb647
Peter Sims <ragtimesims@gmail.com>
2019-09-02 13:14:20 -0400

    routing and landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>Feedbackify</h1>
+			Learn what your users really think!
+		</div>
+	);
+};
+
+export default Landing;
