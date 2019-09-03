29cee05abd2812bee2cf9aa9df106bb7681ab7bd
Peter Sims <ragtimesims@gmail.com>
2019-09-03 08:57:07 -0400

    deployment test script

--- index.js
+++ index.js
@@ -27,5 +27,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	//Express serves prodution assets
+	app.use(express.static('client/build'));
+	//Express will serve index.html if it doesn't recognize the route above
+	const path = require('path');
+	app.get('*', () => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
