45692d8a6d44f433d554e40bdfa02d6a47f1e31a
Peter Sims <ragtimesims@gmail.com>
2019-09-03 09:23:39 -0400

    typo chasing!

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	//Express will serve index.html if it doesn't recognize the route above
 	const path = require('path');
-	app.get('*', () => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
