fc1e7a0dcef703d98ca06b326b493c4251d41165
Peter Sims <ragtimesims@gmail.com>
2019-09-05 09:51:09 -0400

    event logging and refactoring with lodash

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -12,9 +15,24 @@ module.exports = (app) => {
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
-		console.log(req.body);
+		const p = new Path('/api/surveys/:surveyId/:choice');
+
+		const events = _.chain(req.body)
+			.map(({ email, url }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return { email, surveyId: match.surveyId, choice: match.choice };
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.value();
+
+		console.log(events);
+
 		res.send({});
 	});
+
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
