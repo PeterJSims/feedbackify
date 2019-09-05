174e7cb81b6d14642325007e019f42da50ab4706
Peter Sims <ragtimesims@gmail.com>
2019-09-05 14:38:56 -0400

    reverse surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,8 +6,29 @@ class SurveyList extends Component {
 	componentDidMount() {
 		this.props.fetchSurveys();
 	}
+
+	renderSurveys() {
+		return this.props.surveys.reverse().map((survey) => {
+			return (
+				<div className="card blue-grey darken-1" key={survey._id}>
+					<div className="card-content white-text">
+						<span className="card-title">{survey.title}</span>
+						<p>{survey.body}</p>
+						<p className="right">Sent on: {new Date(survey.dateSent).toLocaleDateString()}</p>
+					</div>
+					<div className="card-action">
+						{/* eslint-disable-next-line */}
+						<a>Yes: {survey.yes}</a>
+						{/* eslint-disable-next-line */}
+						<a>No: {survey.no}</a>
+					</div>
+				</div>
+			);
+		});
+	}
+
 	render() {
-		return <div>SurveyList</div>;
+		return <div>{this.renderSurveys()}</div>;
 	}
 }
 
